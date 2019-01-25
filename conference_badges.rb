5819889131b09d2f500f1d6e578c8997403352bb
Johnson Joseph <jhonsonj88@gmail.com>
2019-01-25 17:31:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def badge_maker(name)
+   return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (attendees)
+  attendees.map do |names|
+    "Hello, my name is #{names}."
+  end
+end
+
+def assign_rooms (attendees)
+  attendees.map.with_index do |names, index| "Hello, #{names}! You'll be assigned to room #{index+1}!"
+end 
+end
+
+def printer(attendees)
+batch_badge_creator (attendees).each do |id| 
+  puts id
+end
+assign_rooms(attendees).each do |room| puts room
+end 
+end
+  
